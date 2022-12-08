4fd19c40dea9d4f85d07a58e4fb07bfc24e576fb
j-tomas <j-tomas@outlook.com>
2022-12-08 12:23:35 +0100

    Agregar archivos de proyecto.

---
+++ AgenciaPruebas/Program.cs
@@ -0,0 +1,142 @@
+﻿using ServiceReference;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+
+namespace AgenciaPruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuministroLRFacturasEmitidas suministro = new SuministroLRFacturasEmitidas();
+            LRfacturasEmitidasType listaFacturas = new LRfacturasEmitidasType();
+            FacturaExpedidaType factura = new FacturaExpedidaType();
+
+            // Esta informacion debe coincidir con la identificacion del certificado que se va a usar.
+            PersonaFisicaJuridicaESType persona = new PersonaFisicaJuridicaESType();
+            persona.NIF = "000000000";
+            persona.NombreRazon = "NOMBRE O RAZON SOCIAL";
+
+            CabeceraSii cabecera = new CabeceraSii();
+            cabecera.Titular = persona;
+            suministro.Cabecera = cabecera;
+
+            RegistroSiiPeriodoLiquidacion periodo = new RegistroSiiPeriodoLiquidacion();
+            periodo.Ejercicio = "2022";
+            periodo.Periodo = TipoPeriodoType.Item12;
+            listaFacturas.PeriodoLiquidacion = periodo;
+
+            IDFacturaExpedidaTypeIDEmisorFactura emisor = new IDFacturaExpedidaTypeIDEmisorFactura();
+            emisor.NIF = persona.NIF;
+            IDFacturaExpedidaType idFactura = new IDFacturaExpedidaType();
+            idFactura.IDEmisorFactura = emisor;
+            idFactura.NumSerieFacturaEmisor = "1";
+            idFactura.FechaExpedicionFacturaEmisor = "07-12-2022";
+            listaFacturas.IDFactura = idFactura;
+
+            PersonaFisicaJuridicaType contraparte = new PersonaFisicaJuridicaType();
+            contraparte.NombreRazon = "PRUEBAS S.L.";
+            // El Item debe ser un NIF bien formado.
+            contraparte.Item = "000000000";
+            factura.Contraparte = contraparte;
+            factura.TipoFactura = ClaveTipoFacturaType.F1;
+            factura.FechaOperacion = idFactura.FechaExpedicionFacturaEmisor;
+            factura.ClaveRegimenEspecialOTrascendencia = IdOperacionesTrascendenciaTributariaType.Item01;
+            factura.DescripcionOperacion = "Descripcion de la operacion";
+
+            FacturaExpedidaTypeTipoDesglose desglose = new FacturaExpedidaTypeTipoDesglose();
+            TipoConDesgloseType conDesglose = new TipoConDesgloseType();
+            TipoSinDesglosePrestacionType sinDesglose = new TipoSinDesglosePrestacionType();
+            SujetaPrestacionType sujeta = new SujetaPrestacionType();
+            SujetaPrestacionTypeNoExenta noExenta = new SujetaPrestacionTypeNoExenta();
+            DetalleIVAEmitidaPrestacionType detallePrestacion;
+            detallePrestacion = new DetalleIVAEmitidaPrestacionType();
+            detallePrestacion.BaseImponible = "10.00";
+            detallePrestacion.CuotaRepercutida = "0.04";
+            detallePrestacion.TipoImpositivo = "4.00";
+            noExenta.DesgloseIVA = new DetalleIVAEmitidaPrestacionType[1];
+            noExenta.DesgloseIVA.SetValue(detallePrestacion, 0);
+            noExenta.TipoNoExenta = TipoOperacionSujetaNoExentaType.S1;
+            sujeta.NoExenta = noExenta;
+            sinDesglose.Sujeta = sujeta;
+            conDesglose.PrestacionServicios = sinDesglose;
+            desglose.Item = conDesglose;
+            factura.TipoDesglose = desglose;
+            factura.ImporteTotal = "10.04";
+
+            listaFacturas.FacturaExpedida = factura;
+            suministro.RegistroLRFacturasEmitidas = new LRfacturasEmitidasType[1];
+            suministro.RegistroLRFacturasEmitidas.SetValue(listaFacturas, 0);
+
+            // Cliente SII
+            siiSOAPClient siiService;
+            RespuestaLRFEmitidasType respuesta;
+            try
+            {
+                siiService = new siiSOAPClient(siiSOAPClient.EndpointConfiguration.SuministroFactEmitidasPruebas);
+
+                X509Certificate2 certificado = SeleccionarCertificado("Seleccione un certificado", StoreLocation.CurrentUser, StoreName.My);
+                siiService.ClientCredentials.ClientCertificate.Certificate = certificado;
+                siiService.Open();
+                if (siiService.State == CommunicationState.Opened)
+                {
+                    respuesta = siiService.SuministroLRFacturasEmitidas(suministro);
+                }
+                siiService.Close();
+            }
+            catch (ProtocolException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            catch (WebException ex)
+            {
+                var st = new StreamReader(ex.Response.GetResponseStream());
+                Console.Write(st.ReadToEnd());
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            finally
+            {
+                siiService = null;
+            }
+        }   // static void Main(string[] args)
+
+        private static X509Certificate2 SeleccionarCertificado(string message, StoreLocation location, StoreName name)
+        {
+            X509Store store;
+            X509Certificate2 certificado = null;
+            try
+            {
+                store = new X509Store(name, location);
+                store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+                X509Certificate2Collection certsSelect = store.Certificates.Find(X509FindType.FindByTimeValid, DateTime.Now, true);
+                X509Certificate2Collection certs =
+                    X509Certificate2UI.SelectFromCollection(
+                        certsSelect,
+                        "Selección de certificado",
+                        message,
+                        X509SelectionFlag.SingleSelection, IntPtr.Zero);
+                if (certs.Count > 0)
+                {    //Almacenamos el certificado en nuestro objeto interno
+                    certificado = certs[0];
+                }
+                store.Close();
+                return certificado;
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            finally
+            {
+                store = null;
+            }
+        }   // SeleccionarCertificado()
+
+    }   // class Program
+
+}       // namespace AgenciaPruebas
+
---
+++ Utils - copia.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+//namespace AgenciaPruebas
+//{
+//    internal class Utils
+//    {
+//    }
+//}
+
+namespace AgenciaPruebas
+{
+    public static class Utils
+    {
+        public static string ficheroLog;
+
+        public static void LogToFile(string Message)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(ficheroLog))
+                {
+                    String fichero = System.IO.Path.GetTempPath();
+                    if (!fichero.EndsWith("\\"))
+                        fichero += "\\";
+                    fichero += "Test_SII_";
+                    fichero += System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm");
+                    fichero += ".LOG";
+                    ficheroLog = fichero;
+                }
+                StreamWriter sw = new StreamWriter(ficheroLog, true);
+                StringBuilder sb = new StringBuilder();
+                sb.Append(DateTime.Now.ToString());
+                sb.Append(" - ");
+                sb.Append(Message);
+                sw.WriteLine(sb.ToString());
+                sw.Close();
+                sw = null;
+            }
+            catch (Exception)
+            {
+                // throw ex;
+            }
+        }
+    }
+}
+
